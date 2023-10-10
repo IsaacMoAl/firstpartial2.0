7edfb9012781668e567fac090ae66c8f9cf0717c
IsaacMoAl <145053507+IsaacMoAl@users.noreply.github.com>
2023-10-09 21:13:46 -0600

    p2_road_trip.py

---
+++ p2_road_trip.py
@@ -0,0 +1,7 @@
+D= int(input("Inster the distance"))
+M= float(input("the MPG"))
+P= float(input("cost of gasoline pear gallon"))
+D2= float(input("Days you plan to travel"))
+
+print("The total cost of the travel will be:", D / M * P * D2)
+print("enjoy your travel")
